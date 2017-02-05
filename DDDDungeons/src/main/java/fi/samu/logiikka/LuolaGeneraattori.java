1804ac2298f2d8692f363acbb4834c7c04eb27d9
samuvait <samu.vaittinen@helsinki.fi>
2017-02-05 23:20:11 +0200

    luolageneraattoria siistitty ja korjailtu

--- DDDDungeons/src/main/java/fi/samu/logiikka/LuolaGeneraattori.java
+++ DDDDungeons/src/main/java/fi/samu/logiikka/LuolaGeneraattori.java
@@ -101,8 +101,8 @@ public class LuolaGeneraattori {
         while (!tempHuoneLista.isEmpty()) {
             int sijoitettavaHuone = tempHuoneLista.remove(tempHuoneLista.size() - 1);
             for (int i = 0; i < 100; i++) { // i kertoo montako kertaa huonetta yritetään sijoittaa tyhjään tilaan.
-                int x = rng.nextInt(koko - sijoitettavaHuone - 1);
-                int y = rng.nextInt(koko - sijoitettavaHuone - 1);
+                int x = 1 + rng.nextInt(koko - sijoitettavaHuone - 1); // ei sijoiteta reunoille, jotta käytävä voi alkaa miltä tahansa sivulta
+                int y = 1 + rng.nextInt(koko - sijoitettavaHuone - 1);
                 if (kartta[x][y] == 0) {
                     if (tarkistaOnkoAvoin(x, y, sijoitettavaHuone)) { // tarkistaa onko huoneen paikka pelkästään 0
                         taytaHuone(x, y, sijoitettavaHuone);
@@ -117,17 +117,15 @@ public class LuolaGeneraattori {
     }
 
     public boolean tarkistaOnkoAvoin(int x, int y, int sijoitettavaHuone) { // Tarkistaa, että huone mahtuu kokonaisuudessaan tilaan
-        int viimeinenX = sijoitettavaHuone + x + 2; // viimeisen tarkistettavan täytyy mennä yli, jotta huoneet eivät ole vierekkäin
+        int viimeinenX = sijoitettavaHuone + x + 1; // viimeisen tarkistettavan täytyy mennä yli, jotta huoneet eivät ole vierekkäin
         int aloitusX = x - 1; //viimeisen aloitettavan täytyy alkaa aiemmin samasta syystä
         if (aloitusX < 0) {
             aloitusX = 0;
-            viimeinenX = sijoitettavaHuone + 1; // jos aloitetaan 0
         }
-        int viimeinenY = sijoitettavaHuone + y + 2;
+        int viimeinenY = sijoitettavaHuone + y + 1;
         int aloitusY = y - 1;
         if (aloitusY < 0) {
             aloitusY = 0;
-            viimeinenY = sijoitettavaHuone + 1;
         }
         for (int tarkistettavaX = aloitusX; tarkistettavaX <= viimeinenX && tarkistettavaX < koko; tarkistettavaX++) { // varmista että on taulukon rajoissa
             for (int tarkistettavaY = aloitusY; tarkistettavaY <= viimeinenY && tarkistettavaY < koko; tarkistettavaY++) {
@@ -163,33 +161,7 @@ public class LuolaGeneraattori {
 
     public void arvoAloitusPaikat(Huone huone) { // Arpoo käytävän aloituspaikat
         Random rng = new Random();
-        int x = huone.getX();
-        int y = huone.getY();
-        int huoneenLeveys = huone.getHuoneenLeveys();
-        ArrayList<Integer> vaihtoEhdot = new ArrayList();
-        vaihtoEhdot.addAll(Arrays.asList(0, 1, 2, 3)); // luodaan 4 vaihtoehtoa 0 = huoneen yläosa 1 = vasen sivu 2 = oikea sivu 3 = alaosa
-        int ehto = rng.nextInt(4); // Arvotaan neljästä yksi
-        if (y == 0) { // jos y = 0 ei voida sijoittaa yläosaan
-            if (x == 0) { // jos x = 0 ei voida sijoittaa vasemmalle sivulle
-                ehto = 2 + rng.nextInt(2); // jää 2 ja 3
-            } else {
-                vaihtoEhdot.remove(0); // poistetaan yläosa
-                ehto = vaihtoEhdot.get(rng.nextInt(3));
-            }
-        } else if (x == 0) { // Jos vain x = 0
-            vaihtoEhdot.remove(1); // poistetaan vasen sivu valikoimasta
-            ehto = vaihtoEhdot.get(rng.nextInt(3));
-        } else if (y + huoneenLeveys == koko - 1) { // jos huone menee aivan alareunaan
-            if (x + huoneenLeveys == koko - 1) {
-                ehto = rng.nextInt(2); // ei voida sijoittaa 2 ja 3
-            } else {
-                vaihtoEhdot.remove(3);
-                ehto = vaihtoEhdot.get(rng.nextInt(3));
-            }
-        } else if (x + huoneenLeveys == koko - 1) { // jos huone menee oikeaan reunaan
-            vaihtoEhdot.remove(2);
-            ehto = vaihtoEhdot.get(rng.nextInt(3)); // ei voida sijoittaa alaosaan
-        }
+        int ehto = rng.nextInt(4); // luodaan 4 vaihtoehtoa 0 = huoneen yläosa 1 = vasen sivu 2 = oikea sivu 3 = alaosa
         kayVaihtoehdotLapi(ehto, huone);
     }
 
@@ -225,7 +197,6 @@ public class LuolaGeneraattori {
     }
 
     public void teeKaytavaY(int x, int y) { // aloitetaan liikuttamalla y
-        this.kaytavienMaara++;
         int muutettavaX = x;
         int muutettavaY = y;
         kartta[muutettavaY][muutettavaX] = 1;
@@ -233,29 +204,30 @@ public class LuolaGeneraattori {
         Huone maaranpaa = this.seuraavaHuone; // otetaan maaranpaaksi seuraava huone
         int paaX = maaranpaa.getX() + rng.nextInt(maaranpaa.getHuoneenLeveys() - 1); // arvotaan jokin x-koordinaatti johon käytävä liitetään
         int paaY = maaranpaa.getY() + rng.nextInt(maaranpaa.getHuoneenLeveys() - 1); // arvotaan jokin y-koordinaatti johon käytävä liitetään
-        while (!(paaX == muutettavaX && paaY == muutettavaY)) { // silloin kun ei olla päätekoordinaateissa
-            if (muutettavaY < paaY) { // katsotaan onko lähtö y pienempi vai suurempi, ja liikutaan sen mukaan
-                while (muutettavaY != paaY) { // jos y pienempi kasvatetaan y kunnes saman arvoinen
-                    muutettavaY++;
-                    kartta[muutettavaY][muutettavaX] = 1;
-                }
-            } else {
-                while (muutettavaY != paaY) { //jos y suurempi vähennetään y kunnes saman arvoinen
-                    muutettavaY--;
-                    kartta[muutettavaY][muutettavaX] = 1;
-                }
+        if (muutettavaY < paaY) { // katsotaan onko lähtö y pienempi vai suurempi, ja liikutaan sen mukaan
+            while (muutettavaY != paaY) { // jos y pienempi kasvatetaan y kunnes saman arvoinen
+                muutettavaY++;
+                kartta[muutettavaY][muutettavaX] = 1;
             }
-            if (muutettavaX < paaX) { // sama x:n arvoille y:n jälkeen
-                while (muutettavaX != paaX) {
-                    muutettavaX++;
-                    kartta[muutettavaY][muutettavaX] = 1;
-                }
-            } else {
-                while (muutettavaX != paaX) {
-                    muutettavaX--;
-                    kartta[muutettavaY][muutettavaX] = 1;
-                }
+        } else {
+            while (muutettavaY != paaY) { //jos y suurempi vähennetään y kunnes saman arvoinen
+                muutettavaY--;
+                kartta[muutettavaY][muutettavaX] = 1;
             }
+        }
+        if (muutettavaX < paaX) { // sama x:n arvoille y:n jälkeen
+            while (muutettavaX != paaX) {
+                muutettavaX++;
+                kartta[muutettavaY][muutettavaX] = 1;
+            }
+        } else {
+            while (muutettavaX != paaX) {
+                muutettavaX--;
+                kartta[muutettavaY][muutettavaX] = 1;
+            }
+        }
+        if (paaX == muutettavaX && paaY == muutettavaY) {
+            this.kaytavienMaara++;
         }
     }
 
@@ -267,28 +239,26 @@ public class LuolaGeneraattori {
         Huone maaranpaa = this.seuraavaHuone;
         int paaX = maaranpaa.getX() + rng.nextInt(maaranpaa.getHuoneenLeveys() - 1); // arvotaan jokin x-koordinaatti johon käytävä liitetään
         int paaY = maaranpaa.getY() + rng.nextInt(maaranpaa.getHuoneenLeveys() - 1); // arvotaan jokin y-koordinaatti johon käytävä liitetään
-        while (!(paaX == muutettavaX && paaY == muutettavaY)) { // silloin kun ei olla päätekoordinaateissa
-            if (muutettavaX < paaX) { // jos alku x pienempi kuin pääte x, kasvatetaan kunnes saman arvoinen
-                while (muutettavaX != paaX) {
-                    muutettavaX++;
-                    kartta[muutettavaY][muutettavaX] = 1;
-                }
-            } else { // jos alku x suurempi kuin pääte x, vähennetään kunnes saman arvoinen
-                while (muutettavaX != paaX) {
-                    muutettavaX--;
-                    kartta[muutettavaY][muutettavaX] = 1;
-                }
+        if (muutettavaX < paaX) { // jos alku x pienempi kuin pääte x, kasvatetaan kunnes saman arvoinen
+            while (muutettavaX != paaX) {
+                muutettavaX++;
+                kartta[muutettavaY][muutettavaX] = 1;
             }
-            if (muutettavaY < paaY) { // sama y:n arvoille x:n jälkeen
-                while (muutettavaY != paaY) {
-                    muutettavaY++;
-                    kartta[muutettavaY][muutettavaX] = 1;
-                }
-            } else {
-                while (muutettavaY != paaY) {
-                    muutettavaY--;
-                    kartta[muutettavaY][muutettavaX] = 1;
-                }
+        } else { // jos alku x suurempi kuin pääte x, vähennetään kunnes saman arvoinen
+            while (muutettavaX != paaX) {
+                muutettavaX--;
+                kartta[muutettavaY][muutettavaX] = 1;
+            }
+        }
+        if (muutettavaY < paaY) { // sama y:n arvoille x:n jälkeen
+            while (muutettavaY != paaY) {
+                muutettavaY++;
+                kartta[muutettavaY][muutettavaX] = 1;
+            }
+        } else {
+            while (muutettavaY != paaY) {
+                muutettavaY--;
+                kartta[muutettavaY][muutettavaX] = 1;
             }
         }
         if (paaX == muutettavaX && paaY == muutettavaY) {
